0805245d82af198d776c47f97201625654856c62
felizaparicioplascencia-prog <felizaparicioplascencia@gmail.com>
2026-03-22 14:51:36 -0600

    Update getProducts.tsx

--- src/services/getProducts.tsx
+++ src/services/getProducts.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
+import tipe {product} from"../interfaces/products"
 
 const API_URL = "https://fakestoreapi.com/products";
 
-export const getProducts = async () => {
-  const response = await axios.get(API_URL);
+export const getProducts = async ():promise<product [] > => {
+ 
+  const response = await axios.get("https://fakestoreapi.com/products");
   return response.data;
 };
 
